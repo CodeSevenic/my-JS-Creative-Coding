05d15beabd6bc10f57f51266c4c4a131624ee7b4
CodeSevenic <sbu.audio.production@gmail.com>
2021-10-30 18:23:46 +0200

    code update

--- lab-session-drawing-a-heart.js
+++ lab-session-drawing-a-heart.js
@@ -3,12 +3,21 @@ window.onload = function () {
   var canvas = document.getElementById('hello-world-canvas');
   var context = canvas.getContext('2d');
 
+
+ // context.quadraticCurveTo(controlX,controlY,endX, endY);
+context.beginPath();
+context.strokeStyle = 'red';
+context.lineWidth = 5;
+context.moveTo(500,250);
+context.quadraticCurveTo(600, 50, 700, 250);
+context.stroke();
+
  // context.bezierCurveTo(controlX1,controlY1,controlX1,controlY1,endX, endY);
 context.beginPath();
 context.strokeStyle = 'red';
-context.lineWidth = 10;
+context.lineWidth = 5;
 context.moveTo(200,250);
-context.bezierCurveTo(200, 10, 50, 150, 400, 250);
+context.bezierCurveTo(100, 100, 500, 100, 400, 250);
 context.stroke();
 
 }
