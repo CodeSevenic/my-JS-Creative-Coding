d9dd07c15f0d707f2555c00fdf9651caac701c75
CodeSevenic <sbu.audio.production@gmail.com>
2021-10-30 19:10:55 +0200

    code update

--- lab-session-drawing-a-heart.js
+++ lab-session-drawing-a-heart.js
@@ -1,23 +1,47 @@
-window.onload = function () {
-  // Definition
-  var canvas = document.getElementById('hello-world-canvas');
-  var context = canvas.getContext('2d');
+window.onload = function() {
+	// Definition
+	var canvas = document.getElementById('hello-world-canvas');
+	var context = canvas.getContext('2d');
 
 
- // context.quadraticCurveTo(controlX,controlY,endX, endY);
-context.beginPath();
-context.strokeStyle = 'red';
-context.lineWidth = 5;
-context.moveTo(500,250);
-context.quadraticCurveTo(600, 50, 700, 250);
-context.stroke();
+	drawQuadraticCurve(500, 250, 600, 50, 700, 250, 'blue', 5);
+	drawBezierCurve(200, 250, 100, 100, 500, 100, 400, 250, 'green', 6);
 
- // context.bezierCurveTo(controlX1,controlY1,controlX1,controlY1,endX, endY);
-context.beginPath();
-context.strokeStyle = 'red';
-context.lineWidth = 5;
-context.moveTo(200,250);
-context.bezierCurveTo(100, 100, 500, 100, 400, 250);
-context.stroke();
+	function drawQuadraticCurve(startX, startY, controlX, controlY, endX, endY, curveColor, curveWidth) {
+		var radian = Math.PI / 180;
+		// context.quadraticCurveTo(controlX,controlY,endX, endY);
+		context.beginPath();
+		context.strokeStyle = curveColor;
+		context.lineWidth = curveWidth;
+		context.moveTo(startX, startY);
+		context.quadraticCurveTo(controlX, controlY, endX, endY);
+		context.stroke();
+
+		// Draw the control point as a circle
+		context.beginPath();
+		context.strokeStyle = 'black';
+		context.lineWidth = 10;
+		context.arc(controlX,controlY,5,0 * radian, 360 * radian, false)
+		context.stroke()
+
+		// Draw the lines between control point and path
+		context.beginPath();
+		context.lineWidth = 1;
+		context.moveTo(startX, startY);
+		context.lineTo(controlX, controlY);
+		context.lineTo(endX, endY);
+		context.stroke();
+	}
+
+	function drawBezierCurve(startX, startY, controlX1, controlY1, controlX2, controlY2, endX, endY, curveColor, curveWidth) {
+
+		// context.bezierCurveTo(controlX1,controlY1,controlX2,controlY2,endX, endY);
+		context.beginPath();
+		context.strokeStyle = curveColor;
+		context.lineWidth = curveWidth;
+		context.moveTo(startX, startY);
+		context.bezierCurveTo(controlX1, controlY1, controlX2, controlY2, endX, endY);
+		context.stroke();
+	}
 
 }
