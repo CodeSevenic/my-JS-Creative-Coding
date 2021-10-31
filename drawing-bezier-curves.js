0c897a94c79b79dd4be9802aab7882d62caa5fb7
CodeSevenic <sbu.audio.production@gmail.com>
2021-10-31 22:37:21 +0200

    code update

--- drawing-bezier-curves.js
+++ drawing-bezier-curves.js
@@ -3,12 +3,11 @@ window.onload = function () {
   var canvas = document.getElementById('hello-world-canvas');
   var context = canvas.getContext('2d');
 
- // context.bezierCurveTo(controlX1,controlY1,controlX1,controlY1,endX, endY);
-context.beginPath();
-context.strokeStyle = 'red';
-context.lineWidth = 10;
-context.moveTo(200,250);
-// context.bezierCurveTo(200, 10, 50, 150, 400, 250);
-context.stroke();
-
-}
+  // context.bezierCurveTo(controlX1,controlY1,controlX1,controlY1,endX, endY);
+  context.beginPath();
+  context.strokeStyle = 'red';
+  context.lineWidth = 10;
+  context.moveTo(200, 250);
+  context.bezierCurveTo(200, 10, 50, 150, 400, 250);
+  context.stroke();
+};
