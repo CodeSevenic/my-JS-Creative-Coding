9f9cbfdbc9e3ddfa7ae739f5231fb50ad40ba97a
CodeSevenic <sbu.audio.production@gmail.com>
2021-10-31 22:37:05 +0200

    code update

--- drawing-bezier-curves.js
+++ drawing-bezier-curves.js
@@ -8,7 +8,7 @@ context.beginPath();
 context.strokeStyle = 'red';
 context.lineWidth = 10;
 context.moveTo(200,250);
-context.bezierCurveTo(200, 10, 50, 150, 400, 250);
+// context.bezierCurveTo(200, 10, 50, 150, 400, 250);
 context.stroke();
 
 }
